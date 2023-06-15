9b8f3dca3b351688304d79a3832d8732572767d3
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-15 21:49:17 +0300

    generate js files

---
+++ dist/decorators/autobind.js
@@ -0,0 +1,12 @@
+// autobind decorator
+export function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        }
+    };
+    return adjDescriptor;
+}
+//# sourceMappingURL=autobind.js.map
