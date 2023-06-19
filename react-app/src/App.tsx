a20df00e7b063274c393f6b8a975c8f56d160408
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-19 17:48:36 +0300

    finish new todo

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import {Todo} from "./todo.model";
 
 function App() {
-    const todos = [{id: 't1', text: 'Finish the course'}]
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const todoAddHandler = (text: string) => {
-        console.log(text);
+        const newTodo = {id: Math.random().toString(), text: text}
+        setTodos(prevTodos => [...prevTodos, newTodo]);
     };
 
     return (
