7d4fa1e54e61b61ed3158e70de8967fb68343ee1
Dmitriy Kholodov <dmitriy.kholodov@gmail.com>
2023-06-19 17:58:07 +0300

    delete todo

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -11,10 +11,16 @@ function App() {
         setTodos(prevTodos => [...prevTodos, newTodo]);
     };
 
+    const todoDeleteHandler = (todoId: string) => {
+        setTodos(prevTodos => {
+            return prevTodos.filter(todo => todo.id !== todoId)
+        })
+    };
+
     return (
         <div className="App">
             <NewTodo onAddTodo={todoAddHandler}/>
-            <TodoList items={todos}/>
+            <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
         </div>
     );
 }
--- react-app/src/components/TodoList.tsx
+++ react-app/src/components/TodoList.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 interface TodoListProps {
-    items: { id: string, text: string }[]
+    items: { id: string, text: string }[];
+    onDeleteTodo: (id: string) => void;
 }
 
-const TodoList: React.FC<TodoListProps> = ({items}) => {
-    const renderedTodos = items.map(todo => <li key={todo.id}>{todo.text}</li>);
+const TodoList: React.FC<TodoListProps> = props => {
+    const renderedTodos = props.items.map(todo => (
+        <li key={todo.id}>
+            <span>{todo.text}</span>
+            <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+        </li>
+    ));
 
     return (
         <ul>
